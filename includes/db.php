389749197f43ab8eff95e642d7cd56a2ad6bb8b0
Pankhuri Kushwaha <pankhuri415@gmail.com>
2026-05-09 19:54:38 +0530

    db connect

--- includes/db.php
+++ includes/db.php
@@ -1,6 +1,12 @@
-$conn = mysqli_connect(
-    "sql200.infinityfree.com",
-    "if0_41870268",
-    "Kushwaha2005",
-    "if0_41870268_jobyaari_blog"
-);
+<?php
+$db_host = getenv('DB_HOST') ?: 'sql200.infinityfree.com';
+$db_user = getenv('DB_USER') ?: 'if0_41870268';
+$db_pass = getenv('DB_PASS') ?: 'Kushwaha2005';
+$db_name = getenv('DB_NAME') ?: 'if0_41870268_jobyaari_blog';
+
+$conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
+if ($conn->connect_error) {
+    die('Database connection failed: ' . $conn->connect_error);
+}
+$conn->set_charset('utf8mb4');
+
