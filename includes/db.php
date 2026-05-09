a881eda40a4c0a80624a118fc67e792508ed8efb
Pankhuri Kushwaha <pankhuri415@gmail.com>
2026-05-09 18:11:57 +0530

    updated env

--- includes/db.php
+++ includes/db.php
@@ -1,8 +1,8 @@
 <?php
-$db_host = 'localhost';
-$db_user = 'root';
-$db_pass = '';
-$db_name = 'jobyaari_blog';
+$db_host = getenv('DB_HOST') ?: 'localhost';
+$db_user = getenv('DB_USER') ?: 'root';
+$db_pass = getenv('DB_PASS') ?: '';
+$db_name = getenv('DB_NAME') ?: 'jobyaari_blog';
 
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 if ($conn->connect_error) {
